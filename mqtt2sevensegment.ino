672f112e447fd8f942a19c5b222a96740d9ce8fd
Bertrik Sikken <bertrik@sikken.nl>
2019-07-04 22:30:36 +0200

    Add basic files.

---
+++ mqtt2sevensegment.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#include "ESP8266WiFi.h"
+#include "WiFiManager.h"
+#include "PubSubClient.h"
+#include "TM1637Display.h"
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
+
